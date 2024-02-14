35ee1d3bf02e6261807b4267b58988eb65246ed9
Sharear Ahammed <sharearahammed@gmail.com>
2024-02-14 15:20:24 +0600

    fourth commit

--- js/index.js
+++ js/index.js
@@ -7,7 +7,36 @@
 //     playGroundSection.classList.remove('hidden');
 // }
 
+// function continueGame(){
+//     const alphabet = getARemdomAlphabet();
+//     console.log('Your ramdom alphabet ' , alphabet);
+
+//     // set randomly generated alphabet to the screen 
+//     const currentAlphabet = document.getElementById('current-alphabet');
+//     currentAlphabet.innerText = alphabet;
+
+//     // set background color
+//     setBackgroundColorById(alphabet);
+// }
+
+// function play(){
+//     hideElementById('home-screen');
+//     showElementById('play-ground');
+//     continueGame();
+// }
+
+function cotinueGame(){
+    const alphabet = getRandomAlphabet();
+    console.log('Your ramdom alphabet ',alphabet);
+
+    const currentAlphabet = document.getElementById('current-alphabet');
+    currentAlphabet.innerText = alphabet;
+
+    setBackgroundColorById(alphabet);
+}
+
 function play(){
     hideElementById('home-screen');
     showElementById('play-ground');
+    cotinueGame();
 }
--- js/utility.js
+++ js/utility.js
@@ -1,3 +1,35 @@
+// function hideElementById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.add('hidden');
+// }
+
+// function showElementById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.remove('hidden');
+// }
+
+// function setBackgroundColorById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.add('bg-orange-400');
+// }
+
+
+// function  getARemdomAlphabet(){
+//     // get or creat an alphabet array
+//     const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+//     const alphabets = alphabetString.split('');
+
+//     // get a ramdom index between 0 to 25
+//     const randomNumer = Math.random()*25;
+//     const index = Math.round(randomNumer);
+
+//     const alphabet = alphabets[index];
+//     // console.log(index , alphabet);
+//     return alphabet;
+
+// }
+
+
 function hideElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('hidden');
@@ -5,4 +37,22 @@ function hideElementById(elementId){
 function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
+}
+function getRandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+    // console.log(alphabets);
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    // console.log(index);
+
+    const alphabet = alphabets[index];
+    // console.log(index,alphabet);
+    return alphabet;
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
 }
