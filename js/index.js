1d2eea7f5d936675230bfac34fe51f1bb335455f
Sharear Ahammed <sharearahammed@gmail.com>
2024-02-14 20:30:50 +0600

    fivith commit

--- js/index.js
+++ js/index.js
@@ -25,9 +25,54 @@
 //     continueGame();
 // }
 
-function cotinueGame(){
+// capture keyboard key press
+document.addEventListener('keyup', handleKeyboardButtonPress)
+// --------------------keyboard Key press---------------------
+function handleKeyboardButtonPress(event) {
+    const plyerPressed = event.key;
+    console.log('player pressed', plyerPressed)
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expecterAlphabet = currentAlphabet.toLocaleLowerCase();
+    console.log(plyerPressed, expecterAlphabet);
+
+    if (plyerPressed === expecterAlphabet) {
+        console.log('you get a point')
+
+        // update score
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+
+        // const newScore = currentScore + 1;
+
+        // currentScoreElement.innerText = newScore;
+
+        // start a new round
+        removeBackgroundColorById(expecterAlphabet);
+        cotinueGame();
+    }
+    else {
+        console.log('you missed. you lost a life')
+        // Get the current life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // const newLife = currentLife - 1;
+
+        // currentLifeElement.innerText = newLife;
+
+    }
+}
+
+
+
+
+function cotinueGame() {
     const alphabet = getRandomAlphabet();
-    console.log('Your ramdom alphabet ',alphabet);
+    console.log('Your ramdom alphabet ', alphabet);
 
     const currentAlphabet = document.getElementById('current-alphabet');
     currentAlphabet.innerText = alphabet;
@@ -35,7 +80,7 @@ function cotinueGame(){
     setBackgroundColorById(alphabet);
 }
 
-function play(){
+function play() {
     hideElementById('home-screen');
     showElementById('play-ground');
     cotinueGame();
--- js/utility.js
+++ js/utility.js
@@ -56,3 +56,7 @@ function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
